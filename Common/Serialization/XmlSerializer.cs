07cf28afd0b55bff62633eefd4531f232072d102
Daniil <dadyarri@gmail.com>
2023-12-03 19:26:13 +0300

    add custom xml serializer to utf-8 string

---
+++ Common/Serialization/XmlSerializer.cs
@@ -0,0 +1,27 @@
+﻿using System.Text;
+using System.Xml;
+
+namespace Common.Serialization
+{
+    public class XmlSerializer<T>
+    {
+        private static readonly System.Xml.Serialization.XmlSerializer Serializer = new System.Xml.Serialization.XmlSerializer(typeof(T));
+
+        public static string Serialize(T source)
+        {
+            var stringBuilder = new StringBuilder();
+
+            using (var xmlWriter = XmlWriter.Create(stringBuilder, new XmlWriterSettings
+                   {
+                       Encoding = Encoding.UTF8,
+                       Indent = true,
+                       IndentChars = "  "
+                   }))
+            {
+                Serializer.Serialize(xmlWriter, source);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
