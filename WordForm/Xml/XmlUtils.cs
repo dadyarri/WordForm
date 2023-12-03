774fbff32099981a6a1dc7de93f4924be0b45573
Daniil <dadyarri@gmail.com>
2023-12-03 19:37:17 +0300

    add serialization to adding xml parts

--- WordForm/Xml/XmlUtils.cs
+++ WordForm/Xml/XmlUtils.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Common.Data;
+using Common.Serialization;
 using Office = Microsoft.Office.Core;
 
 namespace WordForm.Xml
 {
     internal class XmlUtils
     {
-        public void WriteCustomXmlPart(Office.CustomXMLParts customParts, string xmlPart)
+        public static void WriteCustomXmlPart(Office.CustomXMLParts customParts, FieldsCollection xmlPart)
         {
-            customParts.Add(xmlPart);
+            var stringData = XmlSerializer<FieldsCollection>.Serialize(xmlPart);
+            customParts.Add(stringData);
         }
     }
 }
