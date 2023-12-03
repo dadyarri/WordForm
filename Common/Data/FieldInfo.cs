9bee11c382474cd35dfb11821103a4de741a760b
Daniil <dadyarri@gmail.com>
2023-12-03 19:25:44 +0300

    add models for custom xml data

---
+++ Common/Data/FieldInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Data
+{
+    public class FieldInfo
+    {
+        public string Id { get; set; }
+        public string VisibleName { get; set; }
+        public string ReplaceTag { get; set; }
+    }
+}
---
+++ Common/Data/FieldsCollection.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Common.Data
+{
+    public class FieldsCollection
+    {
+        public List<FieldInfo> FormFields { get; set; }
+
+        public FieldsCollection()
+        {
+            FormFields = new List<FieldInfo>();
+        }
+    }
+}
