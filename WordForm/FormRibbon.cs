772f4f1e3295c0ab05369dbf4d48e083c2abb7f2
Daniil <dadyarri@gmail.com>
2023-12-03 16:39:22 +0300

    add custom ribbon with required buttons

---
+++ WordForm/FormRibbon.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Office.Tools.Ribbon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WordForm
+{
+    public partial class FormRibbon
+    {
+        private void FormRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+    }
+}
