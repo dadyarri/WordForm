ece6e74d68b0ee484083663a1849f89d2aed6cbd
Daniil <dadyarri@gmail.com>
2023-12-03 19:27:18 +0300

    add new project with some hopping around stuff

---
+++ Runner/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Common.Data;
+using Common.Serialization;
+
+namespace Runner
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var info = new FieldInfo
+            {
+                Id = "some-id",
+                ReplaceTag = "<fio>",
+                VisibleName = "ФИО"
+            };
+
+            var root = new FieldsCollection();
+            
+            for (var i = 0; i < 10; i++)
+            {
+                root.FormFields.Add(info);
+            }
+
+            Console.WriteLine(XmlSerializer<FieldsCollection>.Serialize(root));
+            Console.ReadKey();
+        }
+    }
+}
